53ae52829856806d8bd3c1495d7e2cc2c8ac4027
Fudal96 <fudal22@gmail.com>
2023-07-05 14:40:36 +0200

    working on design

--- client/src/app/pages/a1stories/a1stories.component.ts
+++ client/src/app/pages/a1stories/a1stories.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-a1stories',
   templateUrl: './a1stories.component.html',
   styleUrls: ['./a1stories.component.scss']
 })
-export class A1storiesComponent {
+export class A1storiesComponent implements OnInit{
+  isCollapsed = true;
+
+
+
+
+  ngOnInit(): void {
+    
+  }
+
+
+
+
 
 }
+
+
+
+
+
+
+
